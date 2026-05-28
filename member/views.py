544e5d0203292b69c4c10bdab869bd7b7c979ed5
= <prabin.chaudhary3107@gmail.com>
2026-05-28 07:41:24 +0545

    feat: read member data

--- member/views.py
+++ member/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from member.api.serializer import MemberSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from member.models import Member
 
 # Create your views here.
+# Get member list
+@api_view(['GET'])
+def memberlist(request):
+    member = Member.objects.all()
+    serializer = MemberSerializer(member, many=True)
+    return Response(serializer.data)
+
